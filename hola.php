0ca32b633ffa76a646ebc93e7f8005d980d86d3e
Dante2004 <dantetecseluque@gmail.com>
2024-07-05 09:46:20 -0500

    primer commit

---
+++ hola.php
@@ -0,0 +1,22 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<?PHP
+
+$Nombre = "Joseph";
+
+?>
+
+<h1>HOLA mUNDOI: soy <?php echo $Nombre ?></h1>
+
+
+</body>
+</html>
